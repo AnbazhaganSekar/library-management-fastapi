799e38bf577feff73208a631b32915a0587f2cda
Anbazhagan S <34671765+AnbazhaganSekar@users.noreply.github.com>
2026-04-13 21:48:14 +0530

    Fix table name for Book model

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from .database import Base
 
 
 class Book(Base):
-    __tablename__ = "books"
+
 
     id = Column(Integer, primary_key=True, index=True)
     book_title = Column(String(255), nullable=False)
